a52365039b5cde7f492a588c16678cb0d12eb8fb
BenRoss92 <benjaminjackross@gmail.com>
2017-09-13 16:32:12 +0100

    Run tests using npm command - Configure Gulp to run Selenium Standalone and Webdriver IO - Link Webdriver IO to its configuration file ('wdio.conf.js') - 'mocha' command failing but not an immediate issue - possibly revisit later

--- headline-fetcher/test/indexPageTest.js
+++ headline-fetcher/test/indexPageTest.js
@@ -1,8 +1,10 @@
 var assert = require('assert');
 
 describe('index page', function() {
-  it("displays the headline fetcher title", function () {
+
+  it("displays the headline fetcher title", function() {
     browser.url('/');
     assert.equal(browser.getText('h1'), 'Headline Fetcher');
   });
+
 });
