4a735f4d5d406014bc4d462dd783b80e7c57a259
BenRoss92 <benjaminjackross@gmail.com>
2017-09-20 12:47:20 +0100

    List 2 headlines and article info from API - Make AJAX requests using ‘xmlhttprequest’ package - Hide API Key using ‘dotenv’ package - Refactor tests for listing headlines without search

--- headline-fetcher/test/indexPageTest.js
+++ headline-fetcher/test/indexPageTest.js
@@ -1,10 +1,25 @@
 var assert = require('assert');
 
-describe('index page', function() {
+describe('Index page lists all headlines without explicit search', function() {
+
+  beforeEach(function() {
+    browser.url('/');
+  });
 
   it("displays the headline fetcher title", function() {
-    browser.url('/');
     assert.equal(browser.getText('h1'), 'Headline Fetcher');
+  });
+
+  it("displays a headline when there are results", function () {
+    assert.notEqual(browser.getText('.title'), null);
+  });
+
+  it("displays a summary when there are results", function () {
+    assert.notEqual(browser.getText('.summary'), null);
+  });
+
+  it("displays an editorial when there are results and when there is an editorial", function () {
+    assert.notEqual(browser.getText('.editorial'), null);
   });
 
 });
